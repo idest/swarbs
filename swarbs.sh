4e65f7079768b89413b9bc34171689c18e0e39a5
idest <ie@idest.io>
2021-09-08 08:51:44 -0300

    Only ask for password if necessary.

--- swarbs.sh
+++ swarbs.sh
@@ -31,12 +31,23 @@ welcomemsg() { \
 	dialog --colors --title "Important Note!" --yes-label "All ready!" --no-label "Return..." --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
 	}
 
-getuserandpass() { \
-	# Prompts user for new username an password.
+getuser() { \
+	# Prompts user for desired username
 	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
 		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done
+	done;}
+
+usercheck() { \
+	! { id -u "$name" >/dev/null 2>&1; } ||
+	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten." 14 70
+	}
+
+getpass() { \
+  id -u "$name" >/dev/null 2>&1 &&
+  # If user already exists does nothing.
+  exit ||
+  # Otherwise prompts for a password.
 	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
 	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
 	while ! [ "$pass1" = "$pass2" ]; do
@@ -44,11 +55,6 @@ getuserandpass() { \
 		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
 		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
 	done ;}
-
-usercheck() { \
-	! { id -u "$name" >/dev/null 2>&1; } ||
-	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
-	}
 
 preinstallmsg() { \
 	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit 1; }
@@ -153,11 +159,14 @@ pacman --noconfirm --needed -Syu dialog || error "Are you sure you're running th
 # Welcome user and pick dotfiles.
 welcomemsg || error "User exited."
 
-# Get and verify username and password.
-getuserandpass || error "User exited."
+# Get and verify username.
+getuser || error "User exited."
 
 # Give warning if user already exists.
 usercheck || error "User exited."
+
+# Get and verify password if user does not exist.
+getpass || error "User exited."
 
 # Last chance for user to back out before install.
 preinstallmsg || error "User exited."
